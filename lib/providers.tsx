ad4690086a23e68aa3d2e7cfef174c5e3b1a0805
sh1hsh1nk <13179671+sm86@users.noreply.github.com>
2025-08-15 22:27:50 -0400

    added dynamic wallet

---
+++ lib/providers.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  
+
+  return (
+   <DynamicContextProvider
+      theme="auto"
+      settings={{
+        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID!,
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      {children}
+    </DynamicContextProvider>
+  );
+}
